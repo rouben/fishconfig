0374e8302c70c723aeae7cdaebd99882272b2aee
Rouben Tchakhmakhtchian <rouben@rouben.net>
2017-12-21 05:12:17 -0500

    Made lsvdevs.fish fail gracefully when ZFS VDEVs are not present

--- functions/lsvdevs.fish
+++ functions/lsvdevs.fish
@@ -1,5 +1,10 @@
 function lsvdevs --description 'Lists ZFS VDEVs'
-	for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
-		ls -la /dev/disk/by-vdev/ | grep sd$i
-	end | grep -v part | awk "{print \$11,\$10,\$9}" | sed "s/\.\.\///g"
+	if test -d /dev/disk/by-vdev/
+		for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
+			ls -la /dev/disk/by-vdev/ | grep sd$i
+		end | grep -v part | awk "{print \$11,\$10,\$9}" | sed "s/\.\.\///g"
+	else
+		echo "Doesn't look like ZFS VDEVs are present... bailing!"
+		echo
+	end
 end
