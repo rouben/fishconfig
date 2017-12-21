453509c829fac45c434154b2079e83aa8f994803
Rouben Tchakhmakhtchian <rouben@rouben.net>
2017-12-21 03:41:52 -0500

    Update config.fish with changes from tank

--- config.fish
+++ config.fish
@@ -1,4 +1,21 @@
+# GPG related stuff
 export GPG_TTY=(tty)
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+
+# DEB packaging stuff
 export DEBFULLNAME="Rouben Tchakhmakhtchian"
 export DEBEMAIL="rouben@rouben.net"
+
+# Tackle and Tacklebox related stuff (doesn't really work)
+#set tacklebox_path ~/.tackle
+#set tacklebox_modules virtualfish virtualhooks
+#set tacklebox_plugins grc docker extract
+# All themes are broken
+#set tacklebox_theme urdh
+
+#set tacklebox_path ~/.tackle
+#set tacklebox_modules virtualfish virtualhooks
+#set tacklebox_plugins extract grc pip python up
+#set tacklebox_theme budspencer
+
+#source ~/.tacklebox/tacklebox.fish
