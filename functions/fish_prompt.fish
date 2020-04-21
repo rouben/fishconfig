69789cdd322ee8d80a634d835dc608dbfd0300a3
Rouben Tchakhmakhtchian <rouben@rouben.net>
2020-04-21 16:04:55 -0400

    Fixed function description parameters in fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,4 +1,4 @@
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt --description='Write out the prompt'
 	set -l last_status $status
     set -l normal (set_color normal)
 
@@ -6,25 +6,25 @@ function fish_prompt --description 'Write out the prompt'
     if not set -q -g __fish_classic_git_functions_defined
         set -g __fish_classic_git_functions_defined
 
-        function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+        function __fish_repaint_user --on-variable fish_color_user --description="Event handler, repaint when fish_color_user changes"
             if status --is-interactive
                 commandline -f repaint ^/dev/null
             end
         end
 
-        function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+        function __fish_repaint_host --on-variable fish_color_host --description="Event handler, repaint when fish_color_host changes"
             if status --is-interactive
                 commandline -f repaint ^/dev/null
             end
         end
 
-        function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+        function __fish_repaint_status --on-variable fish_color_status --description="Event handler; repaint when fish_color_status changes"
             if status --is-interactive
                 commandline -f repaint ^/dev/null
             end
         end
 
-        function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
+        function __fish_repaint_bind_mode --on-variable fish_key_bindings --description="Event handler; repaint when fish_key_bindings changes"
             if status --is-interactive
                 commandline -f repaint ^/dev/null
             end
