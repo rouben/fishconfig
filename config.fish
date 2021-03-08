fda8b4bfe642d7664c0a6f2ec80fde5c79873328
Rouben Tchakhmakhtchian <rouben@rouben.net>
2021-03-07 23:44:03 -0500

    Removed tacklebox stuff from fish config

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 # GPG related stuff
 export GPG_TTY=(tty)
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+echo UPDATESTARTUPTTY | /usr/bin/gpg-connect-agent
 
 # DEB packaging stuff
 export DEBFULLNAME="Rouben Tchakhmakhtchian"
@@ -13,16 +14,3 @@ set -gx PATH /snap/bin $PATH
 #eval (/opt/miniconda2/bin/conda shell.fish hook)
 #/opt/miniconda2/bin/conda shell.fish hook | source
 
-# Tackle and Tacklebox related stuff (doesn't really work)
-#set tacklebox_path ~/.tackle
-#set tacklebox_modules virtualfish virtualhooks
-#set tacklebox_plugins grc docker extract
-# All themes are broken
-#set tacklebox_theme urdh
-
-#set tacklebox_path ~/.tackle
-#set tacklebox_modules virtualfish virtualhooks
-#set tacklebox_plugins extract grc pip python up
-#set tacklebox_theme budspencer
-
-#source ~/.tacklebox/tacklebox.fish
